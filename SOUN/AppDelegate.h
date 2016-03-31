8d3fdf1132af77cdb3d6201ef3d501f296c84ebf
Apple Developer <Apple@bogon>
2016-03-31 00:46:15 -0700

    Initial Commit

---
+++ SOUN/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SOUN
+//
+//  Created by Apple Developer on 3/31/16.
+//  Copyright © 2016 Guus Teuwen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SOUN/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SOUN
+//
+//  Created by Apple Developer on 3/31/16.
+//  Copyright © 2016 Guus Teuwen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
